6262ed4449b2e9d68a1b3613c35978dcb658314d
chamberlain-network <46539644+chamberlain-network@users.noreply.github.com>
2024-12-11 13:17:54 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I specialize in analyzing and improving business processes through technology. With a keen eye for detail and a passion for problem-solving, I work closely with stakeholders to understand their needs and design efficient, effective solutions. I have experience in evaluating existing systems, gathering requirements, and implementing technology that aligns with business objectives. My goal is to drive operational improvements by leveraging technology, enhancing system functionality, and ensuring seamless integration for optimal performance. I thrive in collaborative environments, always eager to find innovative ways to address challenges and deliver value.`}
+        {`I specialise in analyzing and improving business processes through technology. With a keen eye for detail and a passion for problem-solving, I work closely with stakeholders to understand their needs and design efficient, effective solutions. I have experience in evaluating existing systems, gathering requirements, and implementing technology that aligns with business objectives. My goal is to drive operational improvements by leveraging technology, enhancing system functionality, and ensuring seamless integration for optimal performance. I thrive in collaborative environments, always eager to find innovative ways to address challenges and deliver value.`}
       </p>
       <div className="my-8">
         <BlogPosts />
